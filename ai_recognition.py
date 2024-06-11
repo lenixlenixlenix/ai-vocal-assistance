7ea0df15e3cfd59172032e8d97ddc3f156560680
lenixlenixlenix <dileron664@gmail.com>
2024-06-11 15:52:33 +0300

    Added AI functions

---
+++ ai_recognition.py
@@ -0,0 +1,96 @@
+import asyncio
+import time
+
+from openai import AsyncOpenAI
+from pathlib import Path
+
+from config import Settings
+
+settings = Settings() 
+
+
+class VocalAssistance():
+    def __init__(self, token):
+        self.client = AsyncOpenAI(api_key=token)
+        
+    async def generate_assistance(self):
+        assistant = await self.client.beta.assistants.create(
+            name="Chat bot",
+            instructions="You are a personal bot that. Answer the questions like i am silly kid.",
+            model="gpt-3.5-turbo"
+        )
+
+        return assistant
+
+    async def answer_question(self, question: str) -> str:
+
+        answer: str = ""
+
+        thread = await self.client.beta.threads.create()
+        assistant = await self.generate_assistance()
+
+        message = await self.client.beta.threads.messages.create(
+            thread_id=thread.id,
+            role="user",
+            content=question
+        )
+
+        run = await self.client.beta.threads.runs.create(
+            thread_id=thread.id,
+            assistant_id=assistant.id,
+            instructions=f"Please answer the question {question}"
+        )
+
+        while True:
+            run_status = await self.client.beta.threads.runs.retrieve(thread_id=thread.id, 
+                                                        run_id=run.id)
+            if run_status.status == "completed":
+                break
+            elif run_status.status == "failed":
+                print("Run failed:", run_status.last_error)
+                answer = "I cannot process your question"
+                break
+            time.sleep(0.5)  # wait for 0.5 second before checking again
+
+        messages = await self.client.beta.threads.messages.list(
+            thread_id=thread.id
+        )
+        answer = messages.data[0].content[0].text.value
+
+        return answer
+
+
+    async def speech_to_text(self, file_path: str) -> str:
+        audio_file = open((file_path), "rb")
+        translation = await self.client.audio.transcriptions.create(
+            model="whisper-1",
+            file=audio_file
+        )
+        return translation.text
+
+    async def text_to_speech(self, text: str) -> None:
+        file = "speech.ogg"
+        response = await self.client.audio.speech.create(
+            model="tts-1",
+            voice="alloy",
+            input=text
+        )
+        response.stream_to_file(Path("files", file))
+
+
+    
+
+
+
+
+    # va = VocalAssistance(settings.open_ai_token)
+    # source_text = await va.speech_to_text("test.mp3")
+    # answer_text = await va.answer_question(source_text)
+    # await va.text_to_speech(answer_text)
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import asyncio
 import logging
 import sys
+import io
+
 from pathlib import Path
+
 
 from aiogram import Bot, Dispatcher, F
 from aiogram.types import Message, Voice
@@ -10,12 +13,26 @@ from aiogram.types import FSInputFile
 
 
 from config import Settings
+from ai_recognition import VocalAssistance
 
 settings = Settings()
 
 dp = Dispatcher()
 
-voice = FSInputFile(Path("files", "sample.ogg"))
+bot = Bot(token=settings.bot_token)
+
+
+voice_helper = VocalAssistance(settings.open_ai_token)
+
+async def save_voice_as_ogg(bot: Bot, voice: Voice) -> str:
+    voice_file_info = await bot.get_file(voice.file_id)
+    voice_ogg_path = Path("files", f"{voice.file_unique_id}.ogg")
+    path = await bot.download_file(voice_file_info.file_path, voice_ogg_path)
+    
+    print(voice_ogg_path)
+
+    return voice_ogg_path
+
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
@@ -23,14 +40,23 @@ async def command_start_handler(message: Message) -> None:
 
 @dp.message(F.voice)
 async def answer_audio(message: Message) -> None:
-    await message.answer_voice(voice=voice)
+    voice_path = await save_voice_as_ogg(bot, message.voice)
+    voice_text = await voice_helper.speech_to_text(voice_path)
+    text_answer = await voice_helper.answer_question(voice_text)
+    
+    await voice_helper.text_to_speech(text_answer)
+
+    voice_file = Path("files", "speech.ogg")
+    voice = FSInputFile(voice_file)
+
+
+    await message.answer_voice(voice)
 
 @dp.message()
 async def answer_default(message: Message) -> None:
     await message.answer("Send voice message")
 
 async def main():
-    bot = Bot(token=settings.bot_token)
 
     await dp.start_polling(bot)
 
